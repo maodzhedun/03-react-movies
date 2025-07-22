45dfe686be2b79d2566b7b1e5edfe620079e3402
maodzhedun <48294837+maodzhedun@users.noreply.github.com>
2025-07-22 18:02:55 +0300

    Modal2

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -8,7 +8,7 @@ interface ModalProps {
   onClose: () => void;
 }
 
-export default function MovieModal({ onClose }: ModalProps) {
+export default function MovieModal({ movie, onClose }: ModalProps) {
   return createPortal(
     <div className={css.backdrop} role="dialog" aria-modal="true">
       <div className={css.modal}>
@@ -20,18 +20,19 @@ export default function MovieModal({ onClose }: ModalProps) {
           &times;
         </button>
         <img
-          src="https://image.tmdb.org/t/p/original/backdrop_path"
-          alt="movie_title"
+          src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+          alt={movie.title}
           className={css.image}
         />
         <div className={css.content}>
-          <h2>movie_title</h2>
-          <p>movie_overview</p>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
           <p>
-            <strong>Release Date:</strong> movie_release_date
+            <strong>Release Date:</strong>
+            {movie.release_date}
           </p>
           <p>
-            <strong>Rating:</strong> movie_vote_average/10
+            <strong>Rating:</strong> {movie.vote_average}/10
           </p>
         </div>
       </div>
